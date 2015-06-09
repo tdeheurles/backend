18e29f72a4d236375fef6fafac5208b854cd1bd5
tdeheurles <tdeheurles@gmail.com>
2015-06-09 11:32:52 +0200

    Prepare build for jenkins, added release

--- build.sh
+++ build.sh
@@ -1,10 +1,18 @@
+#! /bin/bash
+
+if [[ -z $1 ]]; then
+  echo "Need name of build"
+  exit 1
+fi
+
 # Prepare binary
 ./activator clean stage
 
 # copy to the container folder
-mkdir ./container
 mkdir ./container/stage
 
 cp -r 										  \
   ./target/universal/stage  \
   ./container
+
+docker build -t $1 ./container
