79e62dda5ccff42b62a46921a4d4640b0915c7f5
tdeheurles <tdeheurles@gmail.com>
2015-06-09 13:24:58 +0200

    remove docker build from build

--- build.sh
+++ build.sh
@@ -1,22 +1,14 @@
 #! /bin/bash
 
-if [[ -z $1 ]]; then
-  echo "Need name of build"
-  exit 1
-fi
-
-containername=$1
-
 set -e
 
 # Prepare binary
 ./activator clean stage
 
 # copy to the container folder
+mkdir -p ./container
 mkdir -p ./container/stage
 
 cp -r 										  \
   ./target/universal/stage  \
   ./container
-
-docker build -t $1 ./container
